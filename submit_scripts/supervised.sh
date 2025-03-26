f51f3e40449cabd855e1c22b16e68361745312db
dmmiller597 <dmmiller597@gmail.com>
2025-03-26 15:20:40 +0000

    init

--- submit_scripts/supervised.sh
+++ submit_scripts/supervised.sh
@@ -17,9 +17,9 @@ hostname
 echo "#################### QSUB SCRIPT START ####################"
 cat "$0" # print the contents of this file to the log
 echo "####################  QSUB SCRIPT END  ####################"
-conda activate venv
 ROOT_DIR='/SAN/orengolab/functional-families/CATHe2'
 cd $ROOT_DIR
+source /venv/bin/activate
 export WANDB__SERVICE_WAIT=900
 export PYTHONPATH=$PYTHONPATH:$ROOT_DIR
 python ${ROOT_DIR}/src/train.py
