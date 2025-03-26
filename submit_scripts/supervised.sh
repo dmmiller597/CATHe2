1692ce844a4d0d7b587d207b5748d78f7987a018
dmmiller597 <dmmiller597@gmail.com>
2025-03-26 15:15:49 +0000

    init

---
+++ submit_scripts/supervised.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Train supervised model    
+
+#$ -l tmem=127G
+#$ -l gpu=true
+#$ -l gpu_type=a100_80
+#$ -l h_rt=24:00:00
+#$ -S /bin/bash
+#$ -N supervised_model
+#$ -P cath
+#$ -o /SAN/orengolab/functional-families/CATHe2/qsub_logs/
+#$ -wd /SAN/orengolab/functional-families/CATHe2/
+#$ -j y
+date
+hostname
+echo "#################### QSUB SCRIPT START ####################"
+cat "$0" # print the contents of this file to the log
+echo "####################  QSUB SCRIPT END  ####################"
+conda activate venv
+ROOT_DIR='/SAN/orengolab/functional-families/CATHe2'
+cd $ROOT_DIR
+export WANDB__SERVICE_WAIT=900
+export PYTHONPATH=$PYTHONPATH:$ROOT_DIR
+python ${ROOT_DIR}/src/train.py
+date
