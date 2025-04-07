e68e9168ca2b9f560175ce7cd4fe38a11f4bf942
dmmiller597 <dmmiller597@gmail.com>
2025-04-07 17:33:23 +0100

    update qsub

--- submit_scripts/contrastive_model.sh
+++ submit_scripts/contrastive_model.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-# Train contrastive model    
+# Train ProFam
 
-#$ -l tmem=127G
+#$ -l tmem=40G
 #$ -l gpu=true
-#$ -l gpu_type=a100_80
-#$ -l h_rt=24:00:00
+#$ -l gpu_type=(a40|a100|a100_80)
+#$ -l h_rt=12:55:30
 #$ -S /bin/bash
-#$ -N contrastive_model
-#$ -P cath
+#$ -N contrasTED
 #$ -o /SAN/orengolab/functional-families/CATHe2/qsub_logs/
 #$ -wd /SAN/orengolab/functional-families/CATHe2/
+#$ -P cath
 #$ -j y
 date
 hostname
 echo "#################### QSUB SCRIPT START ####################"
 cat "$0" # print the contents of this file to the log
 echo "####################  QSUB SCRIPT END  ####################"
-conda activate venv
+conda activate venvPF
 ROOT_DIR='/SAN/orengolab/functional-families/CATHe2'
 cd $ROOT_DIR
-export WANDB__SERVICE_WAIT=900
+export WANDB__SERVICE_WAIT=300
 export PYTHONPATH=$PYTHONPATH:$ROOT_DIR
 python ${ROOT_DIR}/src/contrastive_train.py
 date
