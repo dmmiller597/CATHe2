b867a63c8283291376b4b7e45b351004675fed76
dmmiller597 <dmmiller597@gmail.com>
2025-04-16 18:42:23 +0100

    set up deterministic runs

--- contrasted/train.py
+++ contrasted/train.py
@@ -11,6 +11,12 @@ from utils import get_logger, set_seed
 from data import ContrastiveDataModule, CATH_LEVEL_NAMES
 from contrasted import ContrastiveCATHeModel
 
+# Set the CUBLAS workspace configuration
+os.environ['CUBLAS_WORKSPACE_CONFIG'] = ':4096:8' # or ':16:8'
+
+# Example of enabling deterministic algorithms (if you haven't already)
+torch.use_deterministic_algorithms(True)
+
 log = get_logger()
 
 @hydra.main(config_path="../config", config_name="contrastive", version_base="1.2")
