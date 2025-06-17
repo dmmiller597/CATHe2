89156dd3c2a165a549d1b5ae427fcd4a299684d6
dmmiller597 <dmmiller597@gmail.com>
2025-06-17 12:50:39 +0100

    init

--- submit_scripts/embeddings.sh
+++ submit_scripts/embeddings.sh
@@ -2,7 +2,7 @@
 
 # Embed sequences using ProtT5
 
-#$ -l tmem=127G
+#$ -l tmem=80G
 #$ -l gpu=true
 #$ -l gpu_type=a100_80
 #$ -l h_rt=24:00:00
@@ -12,8 +12,15 @@
 #$ -o /SAN/orengolab/functional-families/CATHe2/qsub_logs/
 #$ -wd /SAN/orengolab/functional-families/CATHe2/
 #$ -j y
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+echo "#################### JOB DETAILS ####################"
 date
 hostname
+echo "Job ID: ${JOB_ID}"
+echo "Job Name: ${JOB_NAME}"
 echo "#################### QSUB SCRIPT START ####################"
 cat "$0" # print the contents of this file to the log
 echo "####################  QSUB SCRIPT END  ####################"
@@ -21,5 +28,7 @@ ROOT_DIR='/SAN/orengolab/functional-families/CATHe2'
 cd $ROOT_DIR
 source ${ROOT_DIR}/venv/bin/activate
 export HF_HOME=${ROOT_DIR}/model_cache
+echo 'starting python script'
 python ${ROOT_DIR}/scripts/embed_prott5.py --max-tokens 200000
+echo 'python script finished'
 date
