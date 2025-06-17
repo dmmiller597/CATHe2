c8dd8de171f3db79eb7d20254b9ec1820350e1da
dmmiller597 <dmmiller597@gmail.com>
2025-06-17 18:35:02 +0100

    add overlap loss

--- contrasted/losses.py
+++ contrasted/losses.py
@@ -3,7 +3,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch import Tensor
 
-from distances import pairwise_distance
+from distances import pairwise_distance, pairwise_cosine_similarity
+from utils import pairwise_jaccard_similarity, pairwise_overlap_coefficient
 
 
 class SupConLoss(nn.Module):
@@ -44,6 +45,44 @@ class SupConLoss(nn.Module):
         loss = -mean_log_prob_pos.mean()
         return loss 
     
+
+class JaccardLoss(nn.Module):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+    def __call__(self, embeddings, labels):
+        # compute the pairwise jaccard similarity between all the labels in a batch
+        jaccs = torch.tensor(
+            pairwise_jaccard_similarity(labels), device=embeddings.device
+        )
+        cosines = torch.abs(pairwise_cosine_similarity(embeddings))
+        lower_indices = torch.tril_indices(
+            cosines.shape[0], cosines.shape[1], offset=-1
+        )
+        lower_jaccs = jaccs[lower_indices].reshape(-1, 1)
+        lower_cosines = torch.clip(cosines[lower_indices].reshape(-1, 1), 0, 1)
+        return torch.nn.functional.mse_loss(lower_cosines, lower_jaccs)
+
+
+class OverlapLoss(nn.Module):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+    def __call__(self, embeddings, labels):
+        overlap = torch.tensor(
+            pairwise_overlap_coefficient(labels),
+            device=embeddings.device,
+            dtype=embeddings.dtype,
+        )  # compute the pairwise overlap coefficient between all the labels in a batch
+        cosines = pairwise_cosine_similarity(embeddings)
+        lower_indices = torch.tril_indices(
+            cosines.shape[0], cosines.shape[1], offset=-1
+        )
+        lower_overlap = overlap[lower_indices].reshape(-1, 1)
+        lower_cosines = torch.clip(cosines[lower_indices].reshape(-1, 1), 0, 1)
+        return torch.nn.functional.mse_loss(lower_cosines, lower_overlap)
+
+
 class SINCERELoss(nn.Module):
     """
     Supervised InfoNCE REvisited loss with cosine distance. 
